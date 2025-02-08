46420176167ed6f72038e050bb97819b617090e1
watch-me-fly <thi.phoned@gmail.com>
2025-02-08 13:24:18 +0100

    [fix] adapt jsonReaderRepo to modify data

--- AlertSystem/src/main/java/com/safetynet/alertsystem/repository/JsonReaderRepository.java
+++ AlertSystem/src/main/java/com/safetynet/alertsystem/repository/JsonReaderRepository.java
@@ -2,7 +2,6 @@ package com.safetynet.alertsystem.repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.safetynet.alertsystem.model.Data;
-import lombok.Builder;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +9,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Repository;
 
+import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,14 +20,14 @@ public class JsonReaderRepository {
     @Getter private Data data;
 
     @Autowired
-    public JsonReaderRepository(ResourceLoader loader, @Value("${test.json.path}") String jsonPath)
+    public JsonReaderRepository(ResourceLoader loader, @Value("src/main/resources/data.json") String jsonPath)
     {
         this.data = new Data();
         ObjectMapper mapper = new ObjectMapper();
 
         try {
             // load json file from ressources
-            Resource resource = loader.getResource("classpath:" + jsonPath);
+            Resource resource = loader.getResource("file:" + jsonPath);
             LOGGER.log(Level.INFO, "Loading file from : " + resource.getURI());
             // check if resource exists
             if (!resource.exists()) {
@@ -37,6 +37,16 @@ public class JsonReaderRepository {
             data = mapper.readValue(resource.getInputStream(), Data.class);
         } catch (Exception e){
             LOGGER.log(Level.SEVERE, "Error reading json file : ", e);
+        }
+    }
+
+    public void writeData() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(new File("src/main/resources/data.json"), data);
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Error writing json file : ", e);
         }
     }
 }
